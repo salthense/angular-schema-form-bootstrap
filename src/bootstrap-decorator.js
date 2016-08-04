53796cde4919d8f02ae1b0516cd3d863a408a492
Marcel Walwei <mwalwei@saltation.de>
2016-08-04 10:23:27 +0200

    fixup! f|27118|Messdaten|Füge neues Feld für Messdaten hinzu

--- src/bootstrap-decorator.js
+++ src/bootstrap-decorator.js
@@ -191,39 +191,3 @@ app.filter('range', function() {
     return input;
   };
 });
-
-app.directive('measurements', ['$compile', function($compile) {
-  return {
-    controller: ['$scope', '$rootScope', function($scope, $rootScope) {
-      $scope.calculateValue = function (form) {
-        window.measurementFunctions[form.measurementOptions.function]($scope, function(value) {
-          // Saving the value should be done here since it's specific to ASF
-          var model = $scope.model;
-          var pointer;
-          form.key.forEach(function(value) {
-            // ascending down the object path
-            if (typeof model[value] === 'object') {
-              model = model[value];
-            } else {
-              // desired model-attribute found (last key)
-              if (form.key.length == form.key.indexOf(value) + 1) {
-                pointer = value;
-              } else {
-                // create objectpath to the desired model-attribute
-                model[value] = {};
-                model = model[value];
-              }
-            }
-          });
-          model[pointer] = value;
-        });
-      };
-
-      $scope.reset = function(form) {
-        $scope.measurements = [];
-        $('#measurementContainer' + form.key.slice(-1)[0] + ' input')[0].focus();
-        $scope.calculateValue(form);
-      };
-    }]
-  };
-}]);
